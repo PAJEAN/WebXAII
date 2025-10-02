fc32cc7032862aae1bfedae4d9574277dc538769
Jules Leguy <leguy.jules@gmail.com>
2025-10-02 10:26:11 +0200

    update logos

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -4,4 +4,4 @@ export const USER_URL = MAIN_URL + '/api/users';
 export const DATA_URL = MAIN_URL + '/api/data';
 
 // Auth page logos.
-export const LOGO = [];
+export const LOGO = ["assets/res/input/img/enfield.png", "assets/res/input/img/imt.png"];
