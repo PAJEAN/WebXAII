2da9de9d4b762c9483ee58d6cfd98f1d9068de2f
pajean <pierre-antoine.jean@mines-ales.fr>
2025-10-02 09:59:20 +0200

    Edit prod link.

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,5 +1,5 @@
 // const MAIN_URL = window.location.origin;
-const MAIN_URL = process.env.NODE_ENV == 'production' ? 'http://localhost:3000': 'http://localhost:3000';
+const MAIN_URL = process.env.NODE_ENV == 'production' ? 'https://factory.euromov.eu/enfield': 'http://localhost:3000';
 export const USER_URL = MAIN_URL + '/api/users';
 export const DATA_URL = MAIN_URL + '/api/data';
 
