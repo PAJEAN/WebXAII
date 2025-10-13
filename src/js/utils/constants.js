31912d9bb4c03c22ebc3419e08fcc19c54649d92
Jules Leguy <leguy.jules@gmail.com>
2025-10-13 16:12:19 +0200

    protov3

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,7 +1,7 @@
 // const MAIN_URL = window.location.origin;
-const MAIN_URL = process.env.NODE_ENV == 'production' ? 'http://localhost:3000': 'http://localhost:3000';
+const MAIN_URL = process.env.NODE_ENV == 'production' ? 'https://factory.euromov.eu/enfield': 'http://localhost:3000';
 export const USER_URL = MAIN_URL + '/api/users';
 export const DATA_URL = MAIN_URL + '/api/data';
 
 // Auth page logos.
-export const LOGO = [];
+export const LOGO = ["assets/res/img/enfield.png", "assets/res/img/imt.png"];
