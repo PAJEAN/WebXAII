0c14cfa06520d100bcd2a0ebc367125821b6ac2c
pajean <pierre-antoine.jean@mines-ales.fr>
2026-05-21 15:51:45 +0200

    Felipe exp.

--- src/js/pages/p-chain-exp.js
+++ src/js/pages/p-chain-exp.js
@@ -15,7 +15,6 @@ try {
         next_btn: 'next-btn',
         timer: 'timer-div',
         error: 'error-div',
-        more: 'more-btn',
     };
 
     (function() {
@@ -27,18 +26,13 @@ try {
             <style></style>
 
             <div id="${TAG_IDS.main_page}" class="vh-100">
-                <div class="d-flex flex-column justify-content-center align-items-center h-100 container">
+                <div class="d-flex flex-column justify-content-center align-items-center h-100">
                     <div class="row align-items-stretch">
-                        <div class="col d-flex align-items-center flex-column">
-                            <div id="${TAG_IDS.image}">
-                                <img src="assets/datasets/single-kingfisher-bird_xai.jpg" class="icon rounded img-thumbnail" alt="...">
-                            </div>
-                            <div class="w-100">
-                                <button id="${TAG_IDS.more}" type="button" class="btn btn-primary btn-lg text-uppercase w-100 mt-4">Show me more</button>
-                            </div>
+                        <div id="${TAG_IDS.image}" class="col d-flex align-items-center">
+                            <img src="assets/datasets/single-kingfisher-bird_xai.jpg" class="icon rounded img-thumbnail" alt="...">
                         </div>
-                        <div class="col-2 d-flex flex-column justify-content-center align-items-center">
-                            <form id="${TAG_IDS.labels_list}" class="d-flex flex-column gap-2 fs-4">
+                        <div class="col d-flex flex-column justify-content-between">
+                            <form id="${TAG_IDS.labels_list}" class="d-flex flex-column gap-2">
                                 <div class="form-check">
                                     <input class="form-check-input" type="radio" name="labels" id="label1">
                                     <label class="form-check-label" for="label1">Label 1</label>
@@ -72,27 +66,13 @@ try {
                                     <label class="form-check-label" for="label8">Label 8</label>
                                 </div>
                             </form>
-                        </div>
-                        <div class="col d-flex flex-column align-items-center justify-content-center">
-                            <label for="range3" class="form-label fs-2">Confidence of answer : <output for="range4" id="rangeValue" aria-hidden="true">3</output></label>
-                            <input type="range" class="form-range" min="0" max="4" step="1" id="range3" list="range-list">
-
                             <button id="${TAG_IDS.next_btn}" type="button" class="btn btn-primary btn-lg text-uppercase w-100 mt-4">Next</button>
-
                         </div>
                     </div>
                     <div id="${TAG_IDS.timer}" class="my-4 fs-2"></div>
                     <div id="${TAG_IDS.error}" class="text-center text-danger"></div>
                 </div>
             </div>
-
-            <datalist id="range-list">
-            <option value="0">
-            <option value="1">
-            <option value="2">
-            <option value="3">
-            <option value="4">
-            </datalist>
 
         `;
 
@@ -107,17 +87,17 @@ try {
             }
 
             _images() {
-                // if (this.current_view.current_image_index >= this.current_view.images.length) { 
-                //     this._submit();
-                //     return;
-                // }
+                if (this.current_view.current_image_index >= this.current_view.images.length) { 
+                    this._submit();
+                    return;
+                }
 
                 let images_tag = this.content.querySelector(`#${TAG_IDS.image}`);
                 images_tag.innerHTML = `<img src="${this.current_view.images[this.current_view.current_image_index]}" class="icon rounded img-thumbnail" alt="...">`;
             }
 
             _labels() {
-                if (this.current_view.current_image_index >= this.current_view.images.length) {
+                if (this.current_view.current_image_index >= this.current_view.images.length) { 
                     this._submit();
                     return;
                 }
@@ -254,22 +234,6 @@ try {
                     e.preventDefault();
                     this._submit();
                 });
-                let more_btn = this.content.querySelector(`#${TAG_IDS.more}`);
-                more_btn.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    this._images();
-                });
-
-                // This is an example script, please modify as needed
-                const rangeInput = document.getElementById('range3');
-                const rangeOutput = document.getElementById('rangeValue');
-
-                // Set initial value
-                rangeOutput.textContent = rangeInput.value;
-
-                rangeInput.addEventListener('input', function() {
-                    rangeOutput.textContent = this.value;
-                });
             }
          
             connectedCallback () {
@@ -288,7 +252,7 @@ try {
                 /** @type {number | undefined} */
                 this.time_exceeded_timer = false;
 
-                // this._init();
+                this._init();
                 this._init_events();
             }
           
